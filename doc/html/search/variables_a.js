33f3b1d135d0e7e187ed214531e206a56fc0ac25
Vincent Paeder <vpaeder@users.noreply.github.com>
2020-01-20 12:58:10 +0200

    Add files via upload

--- doc/html/search/variables_a.js
+++ doc/html/search/variables_a.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['w_168',['W',['../classtelink_1_1_telink_color.html#a69b8e9e246ea9da6ab6598ed5c36d64f',1,'telink::TelinkColor']]]
+  ['w_169',['W',['../classtelink_1_1_telink_color.html#a69b8e9e246ea9da6ab6598ed5c36d64f',1,'telink::TelinkColor']]]
 ];
--- doc/html/search/variables_b.js
+++ doc/html/search/variables_b.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['y_169',['Y',['../classtelink_1_1_telink_color.html#aefcddf60e57835278ed6d41ddf12003f',1,'telink::TelinkColor']]]
+  ['y_170',['Y',['../classtelink_1_1_telink_color.html#aefcddf60e57835278ed6d41ddf12003f',1,'telink::TelinkColor']]]
 ];
